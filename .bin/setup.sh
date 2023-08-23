33702c57cd64c64b5190c6f54c42b814d8a8e410
Fred-Jan van der Eijken <fred-jan@myonlinestore.com>
2023-08-23 13:26:07 +0200

    Removed oh-mh-zsh dependence + added agnoster ZSH themes

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -2,3 +2,5 @@ git clone http://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.config/zsh/plugins/zsh-syntax-highlighting
+
+curl https://raw.githubusercontent.com/agnoster/agnoster-zsh-theme/master/agnoster.zsh-theme --create-dirs -o ~/.config/zsh/themes/agnoster.zsh-theme
--- .zshrc
+++ .zshrc
@@ -1,5 +1,6 @@
-source ~/.config/zsh/config/oh-my-zsh.zsh
 source ~/.config/zsh/config/dot.zsh
 source ~/.config/zsh/config/mos.zsh
+source ~/.config/zsh/config/prompt.zsh
 source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # must be last
+source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # must be last?
+
